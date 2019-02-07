4b55a378b88862368f4b7e56031fe6483944d5ee
Daniel Kang <d.kang106@gmail.com>
2019-02-06 16:51:50 -0800

    Connected to mlab

---
+++ README.md
@@ -0,0 +1,28 @@
+#Hop-To-It
+-------
+## What is it?
+Full-Stack app using the MERN stack.  A user can use this app to look up companies and see their stock information.  You can create a personal collection of companies that you are interested in.
+
+## Technologies Used:
+HTML5 | CSS3 | Javascript | Git | Github | Heroku | MongoDB | Express | React.js | Node.js | Postman
+
+## ERD
+
+## NPM Packages Installed
+
+## Trello
+
+## Wireframes
+
+## Screenshots
+
+## Code Snippets
+
+## Some problems I ran into:
+
+## Things I didn't get to do/finish:
+
+##GitHub Repo
+https://github.com/dkang003/stock-watch-app
+
+##Deployed App
--- server.js
+++ server.js
@@ -5,4 +5,30 @@ const
     app = express(),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose')
+    mongoose = require('mongoose'),
+    MONGODB_URI = process.env.MONGODB_URI,
+    PORT = process.env.PORT || 3001,
+    path = require('path');
+
+// DB
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true}, (err) => {
+    console.log(err || `Connected to mLab`)
+})
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get('/api', (req,res) => {
+    res.json({ message: "API ROOT" })
+});
+
+// ROUTES
+// app.use('/api/users', usersRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
+
+app.listen(PORT, (err) => {
+    console.log(err || `Server running on port ${PORT}`)
+})
