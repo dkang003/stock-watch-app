31c31e2c0c037ed104135d2bfeea3304f967e95d
Daniel Kang <d.kang106@gmail.com>
2019-02-12 18:32:47 -0800

    working on related stories

--- client/src/components/Stories/index.js
+++ client/src/components/Stories/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default class Stories extends Component {
     state = {
         stories: [],
+        relatedStories: null,
         loading: true
     }
 
@@ -15,32 +16,64 @@ export default class Stories extends Component {
             // debugger
             this.setState({ stories: res.data })
         } catch(err) {
-            console.log(err);
+            debugger
         }
         
     }
 
     render() {
-        let { stories } = this.state;
-        // stories.datetime, headline, source, url, image, related
-
+        let { stories, loading } = this.state;
+        let { stock, company } = this.props;
+        
+        if (stock && loading) {
+            axios.get(`api/data/relatednews/${stock}`)
+            .then(res => {
+                this.setState({ relatedStories: res.data, loading: false })
+                // debugger
+            }).catch(err => {
+                debugger
+            })
+        }
+        // const relatedStoriesExist = this.state.relatedStories;
         return (
             <div>
-                <h1>Top News</h1>
-                <div className="row">
-                { stories.map((story, i) => {
-                    return <div className="card col-sm-12 col-md-4" key={i}>
-                        <div className="card-body text-center">
-                            <h4 className="card-subtitle">{story.datetime}</h4>
-                            <h4 className="card-title">{story.headline}</h4>
-                            <a href={story.url}>{story.url}</a>
-                            <p>Sources: {story.source}</p>
-                            <p>Related: {story.related}</p>
+            {company ? 
+            (
+                <div>
+                    <h1>Related News</h1>
+                    <div className="row">
+                    { this.state.relatedStories.map((story, i) => {
+                        return <div className="card col-sm-12 col-md-4" key={i}>
+                            <div className="card-body text-center">
+                                <h4 className="card-subtitle">{story.datetime}</h4>
+                                <h4 className="card-title">{story.headline}</h4>
+                                <a href={story.url}>{story.url}</a>
+                                <p>Sources: {story.source}</p>
+                                <p>Related: {story.related}</p>
+                            </div>
                         </div>
+                    })}
                     </div>
-                    })
-                }
                 </div>
+            )
+            :(
+                <div>
+                    <h1>Top News</h1>
+                    <div className="row">
+                    { stories.map((story, i) => {
+                        return <div className="card col-sm-12 col-md-4" key={i}>
+                            <div className="card-body text-center">
+                                <h4 className="card-subtitle">{story.datetime}</h4>
+                                <h4 className="card-title">{story.headline}</h4>
+                                <a href={story.url}>{story.url}</a>
+                                <p>Sources: {story.source}</p>
+                                <p>Related: {story.related}</p>
+                            </div>
+                        </div>
+                    })}
+                    </div>
+                </div>
+            )}
             </div>
         )
     }
