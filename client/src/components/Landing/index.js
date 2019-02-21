048db9c3ab1edbfaad03946eb837733e3936e946
Daniel Kang <d.kang106@gmail.com>
2019-02-21 14:42:01 -0800

    changed landing page

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -4,7 +4,7 @@ import './landing.css';
 export default () => {
     return(
     <div className="jumbotron jumbotron-fluid">
-        <div className="container">
+        <div>
             <h1>Stock Watch</h1>
             <p>Keep up with all your favorite stocks!</p>
         </div>
